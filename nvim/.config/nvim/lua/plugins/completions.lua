b619c4e33d0fbfc946d3a59b8fc03be76f165a6b
steven <1085320540@qq.com>
2024-10-09 16:37:37 +0800

    fix check_backspace

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,3 +1,8 @@
+local function check_backspace()
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
 return {
   -- LuaSnip: the Snippet Engine
   {
