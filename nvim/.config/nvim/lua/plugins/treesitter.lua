5a26baabec3cd415aaba1d247c5a8a1af2b3f261
steven <1085320540@qq.com>
2024-11-18 17:09:36 +0800

    delete java from treesitter default installation

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "cpp", "python", "c" },
+			ensure_installed = { "lua", "cpp", "python", "c" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
