4777b8658ff15a76858b253ff78918d743ab0e07
steven <1085320540@qq.com>
2025-02-06 14:35:47 +0800

    feat: install vscode-cpptools

--- install_tools.sh
+++ install_tools.sh
@@ -114,6 +114,33 @@ install_gdb() {
     fi
 }
 
+install_vscode_cpptools() {
+    sleep 2
+    clear
+
+    TOOLS_DIR="$HOME/tools"
+    EXT_DIR="$TOOLS_DIR/vscode-cpptools"
+    VSIX_FILE="$TOOLS_DIR/cpptools-linux-x64.vsix"
+    DOWNLOAD_URL="https://github.com/microsoft/vscode-cpptools/releases/latest/download/cpptools-linux-x64.vsix"
+
+    # 检查 OpenDebugAD7 是否已存在
+    if [ -f "$EXT_DIR/extension/debugAdapters/bin/OpenDebugAD7" ]; then
+        echo -e "\033[1;32m✅ vscode-cpptools 已安装，跳过...\033[0m"
+        return
+    fi
+
+    echo -e "\033[1;36m🔹 正在下载并安装 vscode-cpptools ...\033[0m"
+
+    mkdir -p "$TOOLS_DIR"
+    cd "$TOOLS_DIR"
+
+    wget -O "$VSIX_FILE" "$DOWNLOAD_URL"    # 下载 vscode-cpptools 扩展包
+    unzip -o "$VSIX_FILE" -d "vscode-cpptools"  # 解压到 vscode-cpptools 文件夹
+    rm "$VSIX_FILE"                         # 清理安装包
+
+    echo -e "\033[1;32m✅ vscode-cpptools 安装完成！\033[0m"
+}
+
 # 主执行流程
 mkdir -p $TMP_DIR
 
@@ -122,5 +149,6 @@ install_python
 install_cmake
 install_conda
 install_gdb
+install_vscode_cpptools
 
 rm -rf $TMP_DIR
--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,63 +1,63 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "rcarriga/nvim-dap-ui",
-  },
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "rcarriga/nvim-dap-ui",
+    },
 
-  config = function()
-    require("dapui").setup()
-    local dap = require("dap")
-    local dapui = require("dapui")
+    config = function()
+        require("dapui").setup()
+        local dap = require("dap")
+        local dapui = require("dapui")
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
 
-    -- keymaps
-    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+        -- keymaps
+        vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+        vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+        vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+        vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
 
-    -- C++ Config
-    dap.adapters.cppdbg = {
-      id = "cppdbg",
-      type = "executable",
-      command = "/home/steven/tools/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
-    }
-    dap.configurations.cpp = {
-      {
-        name = "Launch file",
-        type = "cppdbg",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopAtEntry = true,
-      },
-      {
-        name = "Attach to gdbserver :1234",
-        type = "cppdbg",
-        request = "launch",
-        MIMode = "gdb",
-        miDebuggerServerAddress = "localhost:1234",
-        miDebuggerPath = "/usr/bin/gdb",
-        cwd = "${workspaceFolder}",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-      },
-    }
-  end,
+        -- C++ Config
+        dap.adapters.cppdbg = {
+            id = "cppdbg",
+            type = "executable",
+            command = os.getenv("HOME") .. "/tools/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+        }
+        dap.configurations.cpp = {
+            {
+                name = "Launch file",
+                type = "cppdbg",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopAtEntry = true,
+            },
+            {
+                name = "Attach to gdbserver :1234",
+                type = "cppdbg",
+                request = "launch",
+                MIMode = "gdb",
+                miDebuggerServerAddress = "localhost:1234",
+                miDebuggerPath = "/usr/bin/gdb",
+                cwd = "${workspaceFolder}",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+            },
+        }
+    end,
 }
