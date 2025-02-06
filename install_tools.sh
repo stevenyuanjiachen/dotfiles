4fa15ecab789a937f06128311b2d19b0c26cfb8f
steven <1085320540@qq.com>
2025-02-06 13:53:34 +0800

    feat: install gdb

--- install_tools.sh
+++ install_tools.sh
@@ -83,6 +83,37 @@ install_conda(){
     fi
 }
 
+# Install gdb
+install_gdb() {
+    sleep 2
+    clear
+
+    # 检查是否已安装 GDB
+    if ! command -v gdb &> /dev/null; then
+        echo -e "\033[1;36m🔹 正在安装 gdb ...\033[0m"
+        TARGET_GDB_VERSION="15.2"  # 可修改为其他新版本
+
+        # 安装编译依赖
+        sudo apt install -y texinfo libgmp3-dev libmpfr-dev libmpc-dev libncurses-dev
+
+        # 下载并编译 GDB
+        cd $TMP_DIR
+        wget "http://ftp.gnu.org/gnu/gdb/gdb-${TARGET_GDB_VERSION}.tar.gz"
+        tar -zxvf "gdb-${TARGET_GDB_VERSION}.tar.gz"
+        cd "gdb-${TARGET_GDB_VERSION}"
+
+        ./configure --enable-tui
+        make -j$(nproc)
+        sudo make install
+
+        # 清理临时文件
+        cd ..
+        rm -rf "gdb-${TARGET_GDB_VERSION}"*
+    else
+        echo -e "\033[1;32m✅ gdb 已安装，跳过...\033[0m"
+    fi
+}
+
 # 主执行流程
 mkdir -p $TMP_DIR
 
@@ -90,5 +121,6 @@ install_build_essential
 install_python
 install_cmake
 install_conda
+install_gdb
 
 rm -rf $TMP_DIR
